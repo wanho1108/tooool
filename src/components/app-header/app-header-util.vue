6f6bbbed5096a12f09cf38da73025800a70511d4
wanho1108 <wanho1108@gmail.com>
2018-03-13 23:41:56 +0900

    About 라우터 기능 개선

--- src/components/app-header/app-header-util.vue
+++ src/components/app-header/app-header-util.vue
@@ -1,11 +1,31 @@
 <template>
   <div class="header__util">
-    <router-link :to="$route.name === 'AppAbout' ? '/' : '/about'" active-class="is-selected" class="header__util-anchor">About</router-link>
+    <router-link to="/about" active-class="is-selected" class="header__util-anchor" @click.native="routerLink()">About</router-link>
   </div>
 </template>
 
 <script>
   export default {
     name: 'app-header-util',
+    data() {
+      return {
+        to: null,
+      };
+    },
+    methods: {
+      routerLink() {
+        if (this.to === null) this.to = '/';
+        this.$router.push(this.to);
+      },
+    },
+    watch: {
+      $route(to, from) {
+        if (to.path !== '/about' && from.path !== '/about') {
+          this.to = '/about';
+        } else {
+          this.to = from.path;
+        }
+      },
+    },
   };
 </script>
